ac038e38252e7573807f9718777f68acc0f9d567
efrain105 <efrainclochard@hotmail.com>
2025-09-08 12:26:55 -0600

    Update CORS allowed origins and frontend base URL to use the API domain

--- src/main/java/com/cut/cardona/security/SecurityConfiguration.java
+++ src/main/java/com/cut/cardona/security/SecurityConfiguration.java
@@ -31,7 +31,7 @@ public class SecurityConfiguration {
 
     private final SecurityFilter securityFilter;
 
-    @Value("${app.cors.allowed-origins:http://localhost:3000,http://192.168.100.2:3000,https://perritoscut-app-front.vercel.app,http://18.205.240.142, https://www.perritoscut.online}")
+    @Value("${app.cors.allowed-origins:http://localhost:3000,http://192.168.100.2:3000,https://perritoscut-app-front.vercel.app,http://18.205.240.142, https://api.perritoscut.online}")
     private String allowedOriginsCsv;
 
     @Bean
